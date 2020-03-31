eba18a5473ae641d637cdea108345cf79be9b11e
Gu Haoping <ghp525571319@gmail.com>
2020-03-31 22:45:26 +0800

    update: perfect login & extraction

--- router/views.py
+++ router/views.py
@@ -1,9 +1,13 @@
+# coding: utf-8
 from knext_site.router import app
-from flask import session, request
-from knext_site.common import log_in
+from flask import session, request, make_response
+from knext_site.common import log_in, response,api
+from knext_site.lib.knext_rpc_client import ExtractionClient
+from knext_site.db.models import User
 import time
 
 API_PREFIX = '/api/v1'
+client = ExtractionClient()
 
 
 @app.route('/')
@@ -11,22 +15,54 @@ def home():
     '''
     for test usage
     '''
-    print 'aaa'
-    print session
-    session['guhaoping'] = time.time()
-    return 'success'
+    return response.ok('welcome to knext site')
 
 
-@app.route('/get')
-def get_session():
-    return str(session['guhaoping'])
-    
+@app.route(API_PREFIX + '/signup', methods=['post'])
+def signup():
+    input_json = request.get_json()
+    username = input_json.get('username')
+    password = input_json.get('password')
+    try:
+        user = User.get_by_username(username)
+        if user:
+            return response.error('user already exists')
+        user = User(input_json)
+        user.save()
+        session[user.username] = {'status': 0}
+        resp = make_response(response.ok())
+        resp.set_cookie('session_id', user.username)
+        return resp
+    except:
+        return response.error('server error, please contact admin')
+
 
 @app.route(API_PREFIX + '/login', methods=['post'])
 def login():
     input_json = request.get_json()
-    username = input_json.get(username)
-    password = input_json.get(password)
+    username = input_json.get('username')
+    password = input_json.get('password')
+    user = User.login(username, password)
+    if not user:
+        return response.error('invalid username or password')
+    cookie = request.cookies.get('session')
+    session[cookie] = {'username': user.username}
+    resp = make_response(response.ok())
+    return resp
+
+
+@app.route(API_PREFIX + '/extract', methods=['post'])
+@api.require_login
+def extract():
+    input_json = request.get_json()
+    text = input_json.get('text')
+    print text
+    res = client.get_extraction(text.encode('utf-8'), 'knext_site')
+    print res
+    return response.ok(res)
+
+
+
 
 
 
